80064c777adcdf092534f46cefb23064791684ba
lunox94 <gmrlunox94@gmail.com>
2025-01-14 16:44:36 +0100

    Add array based stack

---
+++ data_structures/array_based_stack.rb
@@ -0,0 +1,61 @@
+class Stack
+  attr_accessor :array, :top
+
+  def initialize
+    self.array = Array.new(1)
+    self.top = -1
+  end
+
+  def empty?
+    top == -1
+  end
+
+  def peek
+    empty? ? nil : array[top]
+  end
+
+  def pop
+    if empty?
+      return nil
+    else
+      self.top -= 1
+      return array[top+1]
+    end
+  end
+
+  def push(element)
+    self.top += 1
+
+    unless top < array.size
+      expand
+    end
+    
+    array[top] = element
+  end
+
+  def to_s
+    array[0..top].to_s
+  end
+
+  private
+
+  def expand
+    new_array = Array.new(array.size * 2)
+    
+    array.each_with_index { |item, index| new_array[index] = item }
+
+    self.array = new_array
+  end
+end
+
+stack = Stack.new
+puts stack.empty?
+stack.push(5)
+stack.push(6)
+stack.push(0)
+puts stack
+puts stack.pop
+puts stack.pop
+stack.push 25
+puts stack
+puts stack.empty?
