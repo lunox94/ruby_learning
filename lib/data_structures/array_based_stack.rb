3c6e2b03146ad8b0218458f815e021aa204fd67d
lunox94 <gmrlunox94@gmail.com>
2025-01-15 17:38:19 +0100

    Refactor Stack class to use instance variables for array and top

--- lib/data_structures/array_based_stack.rb
+++ lib/data_structures/array_based_stack.rb
@@ -1,49 +1,47 @@
 class Stack
-  attr_accessor :array, :top
-
   def initialize
-    self.array = Array.new(1)
-    self.top = -1
+    @array = Array.new(1)
+    @top = -1
   end
 
   def empty?
-    top == -1
+    @top == -1
   end
 
   def peek
-    empty? ? nil : array[top]
+    empty? ? nil : @array[@top]
   end
 
   def pop
     if empty?
       return nil
     else
-      self.top -= 1
-      return array[top+1]
+      @top -= 1
+      return @array[@top+1]
     end
   end
 
   def push(element)
-    self.top += 1
+    @top += 1
 
-    unless top < array.size
+    unless @top < @array.size
       expand
     end
     
-    array[top] = element
+    @array[@top] = element
   end
 
   def to_s
-    array[0..top].to_s
+    @array[0..@top].to_s
   end
 
   private
 
   def expand
-    new_array = Array.new(array.size * 2)
+    new_array = Array.new(@array.size * 2)
     
-    array.each_with_index { |item, index| new_array[index] = item }
+    @array.each_with_index { |item, index| new_array[index] = item }
 
-    self.array = new_array
+    @array = new_array
   end
 end
