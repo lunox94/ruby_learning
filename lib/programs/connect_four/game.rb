95d552025f32a9c58e3a7aed87b26fd7ce62d12a
lunox94 <gmrlunox94@gmail.com>
2025-02-02 22:22:53 +0100

    Add Game class to manage Connect Four game flow

---
+++ lib/programs/connect_four/game.rb
@@ -0,0 +1,45 @@
+require_relative "grid"
+require_relative "player"
+
+class Game
+  attr_reader :players, :grid, :current_player
+
+  def initialize
+    @players = [Player.new("Player 1", "🔵"), Player.new("Player 2", "🔴", Grid::PLAYER_TWO)]
+    @grid = Grid.new
+  end
+  
+  def start
+    @current_player = @players.first
+
+    until @grid.full? || @grid.winner?
+      display_grid
+      column = @current_player.choose_column
+      @grid.move(column, @current_player.key)
+      swap_players
+    end
+
+    display_grid
+    if @grid.winner?
+      winner = @current_player == @players.first ? @players.last : @players.first
+      puts "Congratulations, #{winner.name}! You win!"
+    else
+      puts "It's a draw!"
+    end
+  end
+
+  private
+
+  def display_grid
+    system "clear" or system "cls"
+    mapped_grid = @grid.board.map { |row| row.map { |cell| cell.nil? ? "⚪️" : cell == Grid::PLAYER_ONE ? players[0].piece : players[1].piece }.join(" ") }
+    mapped_grid = mapped_grid.map { |row| ["┃", row, "┃"].join(" ") }
+    puts mapped_grid
+    puts "┗━━━━━━━━━━━━━━━━━━━━━━┛"
+    puts "  0  1  2  3  4  5  6"
+  end
+
+  def swap_players
+    @current_player = @current_player == @players.first ? @players.last : @players.first
+  end
+end
